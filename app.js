f9d5d6ade11ec14e69eea17ee93d29492df7ea7e
yousefborini02 <yousefborinii@gmail.com>
2024-06-05 19:00:18 +0300

    Add callGender function

--- app.js
+++ app.js
@@ -30,10 +30,59 @@
 
 //Task 2
 
-let name = prompt("Enter your name :");
-let gender = prompt("Your gender : ").toLowerCase();
+// let name = prompt("Enter your name :");
+// let gender = prompt("Your gender : ").toLowerCase();
 
-if (gender==="male")
+// if (gender==="male")
+//         {
+//             alert("Welcome Mr "+ name);
+//         }
+//     else if (gender==="female")
+//         {
+//             alert("Welcome Ms "+ name);
+//         }
+//         else
+//         { 
+//             while ((gender!== "male") && (gender!== "female")){
+//                 gender= prompt("Your gender : (male,female) ").toLowerCase();
+//             }   
+//         }
+
+//     let confirming = confirm("do you want coffee or donuts or ice cream or bakery ?")
+//     if (confirming===true)
+//         {
+//            let order = prompt("Enter your order : (coffee , donuts , ice cream , bakery) ");
+//            alert("your order is getting prepared");
+//         }
+//         console.log("user name : "+ name + "user order : " + order);
+
+
+//         let type = typeof(order);
+//         let arr = [name , gender , type , order];
+
+//         for(let i=0;i<arr.length;i++)
+//             {
+//                 console.log(arr[i]);
+//             }
+
+
+
+
+
+
+
+
+
+
+
+//Task 3
+
+let name = prompt("Enter your name :");
+
+function callGender(){
+   let gender = prompt("Your gender : ").toLowerCase();
+
+   if (gender==="male")
         {
             alert("Welcome Mr "+ name);
         }
@@ -54,6 +103,10 @@ if (gender==="male")
            let order = prompt("Enter your order : (coffee , donuts , ice cream , bakery) ");
            alert("your order is getting prepared");
         }
+}
+
+        callGender();
+
         console.log("user name : "+ name + "user order : " + order);
 
 
