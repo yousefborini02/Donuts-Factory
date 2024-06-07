274289b8cfd363e3d5cf651af3f749e3e7f4a295
yousefborini02 <yousefborinii@gmail.com>
2024-06-07 21:18:19 +0300

    Adding domTask

--- app.js
+++ app.js
@@ -77,11 +77,69 @@
 
 //Task 3
 
+// let name = prompt("Enter your name :");
+
+// function callGender(){
+//    let gender = prompt("Your gender : ").toLowerCase();
+
+//    if (gender==="male")
+//         {
+//             alert("Welcome Mr "+ name);
+//         }
+//     else if (gender==="female")
+//         {
+//             alert("Welcome Ms "+ name);
+//         }
+//         else
+//         { 
+//             while ((gender!== "male") && (gender!== "female")){
+//                 gender= prompt("Your gender : (male,female) ").toLowerCase();
+//             }   
+//         }
+
+//     let confirming = confirm("do you want coffee or donuts or ice cream or bakery ?")
+//     if (confirming===true)
+//         {
+//            let order = prompt("Enter your order : (coffee , donuts , ice cream , bakery) ");
+//            alert("your order is getting prepared");
+//         }
+// }
+
+//         callGender();
+
+//         console.log("user name : "+ name + "user order : " + order);
+
+
+//         let type = typeof(order);
+//         let arr = [name , gender , type , order];
+
+//         for(let i=0;i<arr.length;i++)
+//             {
+//                 console.log(arr[i]);
+//             }
+
+
+
+
+
+
+
+
+
+//task4 
+
 let name = prompt("Enter your name :");
+
+let information=document.getElementsByClassName("information")[0];
+let parName=document.createElement("p");
+parName.innerHTML=name;
+information.appendChild(parName);
+
+let ord=document.createElement("ol");
+information.appendChild(ord);
 
 function callGender(){
    let gender = prompt("Your gender : ").toLowerCase();
-
    if (gender==="male")
         {
             alert("Welcome Mr "+ name);
@@ -96,19 +154,29 @@ function callGender(){
                 gender= prompt("Your gender : (male,female) ").toLowerCase();
             }   
         }
-
-    let confirming = confirm("do you want coffee or donuts or ice cream or bakery ?")
-    if (confirming===true)
-        {
-           let order = prompt("Enter your order : (coffee , donuts , ice cream , bakery) ");
-           alert("your order is getting prepared");
-        }
+        let genderHtml=document.createElement("li");
+        genderHtml.innerHTML=gender;
+        ord.appendChild(genderHtml);
 }
 
         callGender();
 
-        console.log("user name : "+ name + "user order : " + order);
+        let age=prompt("Enter your age");
+        let ageHtml=document.createElement("li");
+        ageHtml.innerHTML=age;
+        ord.appendChild(ageHtml);
 
+        let confirming = confirm("do you want coffee or donuts or ice cream or bakery ?")
+        if (confirming===true)
+          {
+           let order = prompt("Enter your order : (coffee , donuts , ice cream , bakery) ");
+           alert("your order is getting prepared");
+           let orderHtml=document.createElement("li");
+           orderHtml.innerHTML=order;
+           ord.appendChild(orderHtml);
+          }
+
+        console.log("user name : "+ name + "user order : " + order);
 
         let type = typeof(order);
         let arr = [name , gender , type , order];
@@ -117,6 +185,10 @@ function callGender(){
             {
                 console.log(arr[i]);
             }
+
+           
+
+           
 
 
 
